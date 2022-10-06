c739503dc2fc8a0eda5df6a134da14bf0a8ec7db
dorofeevs <aleksandr.dorofeev@akvelon.com>
2022-10-06 17:25:22 +0400

    Remove comment

--- UIKitHw/Extensions/Delegate+Extension.swift
+++ UIKitHw/Extensions/Delegate+Extension.swift
@@ -22,7 +22,7 @@ extension BookViewController: UIPickerViewDelegate {
     for font in Fonts.fonts {
       keys.append(font.key)
     }
-    let fonts = [Constants.arialHebrewFont, Constants.gothicNeoFont, Constants.courierFont, Constants.typewriterFont, Constants.helveticaNeueFont]
+    let fonts = [Constants.arialHebrewFont, Constants.gothicNeoFont, Constants.courierFont, Constants.typewriterFont, Constants.helveticaNeueFont, Constants.didotFont]
     switch keys[row] {
     case let fontName where fontName == Constants.arialHebrewFont:
       changeFontAndSizeAction(name: fontName, isBold: false)
@@ -42,5 +42,3 @@ extension BookViewController: UIPickerViewDelegate {
     currentFont = keys[row]
   }
 }
-
-//      let fonts = [Constants.arialHebrewFont, Constants.gothicNeoFont, Constants.courierFont, Constants.typewriterFont, Constants.helveticaNeueFont, Constants.didotFont]
